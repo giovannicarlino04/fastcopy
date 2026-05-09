cdb347b5cac3cab0422edeabcae2d4a6d7f82cad
Giovanni Carlino <carlinogiovanni04@gmail.com>
2026-05-09 20:05:27 +0200

    Fix backslash

--- src/fastcopy.c
+++ src/fastcopy.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  printf("file %s copied to %s", source_path.data, dest_path.data);
+  printf("file %s copied to %s\n", source_path.data, dest_path.data);
 
   arena_destroy(&arena);
 
